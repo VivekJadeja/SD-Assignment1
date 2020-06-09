3b11a1be61dd6bd41169f5efc4a79ed8a6da9274
Daniel Evans <53202097+dkulinov@users.noreply.github.com>
2020-06-09 11:38:21 -0500

    add required libraries

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var session = require('express-session');
+var app = express();
+var port = process.env.PORT || 3000;
+var path = require('path');
+var db = require('./server/db.js');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//used to get the correct directory which is the root directory 
+app.use(express.static(path.join(__dirname, './')));
+
+//allows us to use data send from html forms using req.body.name
+app.use(express.urlencoded({ extended: false }));
+
+//each user has a different session so we can keep track of them no matter where they are on the site
+app.use(session({secret: "SecretKey"}));
+
+//redefine views folder to the right path
+app.set('views', path.join(__dirname, './views'));
