c73e163df232b6a60553bff2ae17cf19e98a9e04
Daniel Evans <53202097+dkulinov@users.noreply.github.com>
2020-06-17 12:48:39 -0500

    pull data from front end

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.use(session({secret: "SecretKey"}));
 //redefine views folder to the right path
 app.set('views', path.join(__dirname, './views'));
 
-function user(email, f_name, has_requested_quote)
+function user(email, f_name, has_requested_quote, has_filled_out_profile)
 {
     this.email = email;
     this.f_name = f_name;
     this.has_requested_quote = has_requested_quote;
+    this.has_filled_out_profile = has_filled_out_profile;
 }
 
 // ----------------------------- ROUTES -------------------------------- //
@@ -47,13 +48,39 @@ app.post('/signup', function(req, res){
         email : req.body.email,
         password : req.body.password 
     };
-    db.signUp(data, function(err)
-    {
-        if(err)
-            res.render('errorPage', {message: "Something went wrong. Please try again"});
-        else
-            res.redirect('/login');
-    });
+    console.log('-----POST Request of Signup ------');
+    console.log(data.email);
+    console.log(data.password);
+    // db.signUp(data, function(err)
+    // {
+    //     if(err)
+    //         res.render('errorPage', {message: "Something went wrong. Please try again"});
+    //     else
+    //         res.redirect('/login');
+    // });
+    res.send('<h1>success! check console for info</h1><a href="/">Home</a>');
+});
+
+app.get('/registerProfile', function(req, res)
+{
+    // if(!req.session.user)
+    //     res.render('errorPage', "You need to be logged in");
+    // else if(req.session.user.has_filled_out_profile)
+    //     res.render('errorPage', "You have already provided your information");
+    // else
+        res.sendFile(path.join(__dirname, '/views/registerProfile.html'));
+});
+
+app.post('/registerProfile', function(req, res)
+{
+    res.send('<h1>success! check console for info</h1><a href="/">Home</a>');
+    console.log('-----POST Request of Register Profile ------');
+    console.log(req.body.fullName);
+    console.log(req.body.streetAddress1);
+    console.log(req.body.streetAddress2);
+    console.log(req.body.city);
+    console.log(req.body.state);
+    console.log(req.body.zipCode);    
 });
 
 app.get('/login', function(req, res){
@@ -64,7 +91,14 @@ app.get('/login', function(req, res){
 });
 
 app.post('/login', function(req, res){
-
+    res.send('<h1>success! check console for info</h1><a href="/">Home</a>');
+    var data = {
+        email : req.body.email,
+        password : req.body.password 
+    };
+    console.log('-----POST Request of Login ------');
+    console.log(data.email);
+    console.log(data.password);
 });
 
 app.get('/requestQuote', function(req, res)
@@ -157,7 +191,6 @@ app.get('/userHome', function(req, res)
         res.render('userHome.ejs', {customer: customer});
     }
 });
-
 
 app.get('*', function(req, res){
     res.render('errorPage', {message: "This page doesn't exist."});
