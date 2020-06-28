57197f56204bf4078bb392335a542cac9b52add3
Mayoorshardha <60448086+Mayoorshardha@users.noreply.github.com>
2020-06-28 12:04:19 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -5,20 +5,15 @@ let pool = mysql.createPool({
     host: '',
     user: 'admin',
     password: '',
-    database: '' 
+    database: ''
 });
 
-signUp = function(data, callback)
-{
-    pool.getConnection(function(err, connection)
-    {
-        if(err)
-        {
+signUp = function(data, callback) {
+    pool.getConnection(function(err, connection) {
+        if (err) {
             callback(true);
             return;
-        }
-        else
-        {
+        } else {
             sql = "INSERT INTO UserCredentials VALUES(";
             sql += email;
             sql += ",";
@@ -26,16 +21,12 @@ signUp = function(data, callback)
             sql += ")";
             sql += ";";
             // FINISH THE QUERY ONCE DB IS CREATED
-            connection.query(sql, function(err, result)
-            {
-                if(err)
-                {
+            connection.query(sql, function(err, result) {
+                if (err) {
                     connection.release();
                     callback(true);
                     return;
-                }
-                else
-                {
+                } else {
                     callback(false);
                 }
             });
@@ -43,3 +34,84 @@ signUp = function(data, callback)
     });
 }
 module.exports.signUp = signUp;
+
+getCustomerAddress = function(email, callback) {
+
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            callback(true);
+            return;
+        } else {
+            let sql = "SELECT address_1, address_2, city, state, zipCode, FROM ClientInformation WHERE email = ";
+            sql += email;
+            // FINISH THE QUERY ONCE DB IS CREATED
+            connection.query(sql, function(err, result) {
+                connection.release();
+                if (err) {
+                    callback(true);
+                    return;
+                } else {
+                    callback(false, result);
+                }
+            });
+        }
+    });
+}
+module.exports.getCustomerAddress = getCustomerAddress;
+
+getCustomerHistory = function(email, callback) {
+
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            callback(true);
+            return;
+        } else {
+            let sql = "SELECT ClientInformation.*, FuelQoute.* FROM ClientInformation,FuelQoute WHERE (ClientInformation.email = ? AND FuelQoute.email=ClientInformation.email);"
+            let res = sql.replace("?", email);
+            // FINISH THE QUERY ONCE DB IS CREATED
+            connection.query(res, function(err, result) {
+                connection.release();
+                if (err) {
+                    callback(true);
+                    return;
+                } else {
+                    callback(false, result);
+                }
+            });
+        }
+    });
+}
+module.exports.getCustomerHistory = getCustomerHistory;
+
+requestQuote = function(data, callback) {
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            callback(true);
+            return;
+        } else {
+            sql = "INSERT INTO FuelQoute VALUES(";
+            sql += data.email;
+            sql += ",";
+            sql += data.requestedDate;
+            sql += ",";
+            sql += data.deliveryDate;
+            sql += ",";
+            sql += data.gallons;
+            sql += ",";
+            sql += data.price;
+            sql += ")";
+            sql += ";";
+            // FINISH THE QUERY ONCE DB IS CREATED
+            connection.query(sql, function(err, result) {
+                connection.release();
+                if (err) {
+                    callback(true);
+                    return;
+                } else {
+                    callback(false);
+                }
+            });
+        }
+    });
+}
+module.exports.requestQuote = requestQuote;
