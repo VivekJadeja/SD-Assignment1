56f93b331e845dca67e5466f93d179c64fd57df3
VivekJadeja <57203251+VivekJadeja@users.noreply.github.com>
2020-06-28 12:57:22 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -115,3 +115,90 @@ requestQuote = function(data, callback) {
     });
 }
 module.exports.requestQuote = requestQuote;
+
+loginAndGetInfo = function(data, callback){
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            callback(true);
+            return;
+        } else {
+            let sql = "SELECT UserCredentials.email, ClientInformation.fullName, ClientInformation.historyExists FROM UserCredentials, ClientInformation WHERE UserCredentials.email = ClientInformation.email AND UserCredentials.email = "
+            sql += email;
+            sql+= " AND UserCredentials.password = ";
+            sql += password;
+            // when a user signs up, we should automatically create and entry into CleintInformation for their email, while everything else
+            //is blank.";
+            // FINISH THE QUERY ONCE DB IS CREATED
+            connection.query(sql, function(err, result) {
+                connection.release(); //release connection after it is used in order to not keep it open
+                if (err) {
+                    callback(true);
+                    return;
+                } else {
+                    callback(false, result);
+                }
+            });
+        }
+    });
+}
+module.exports.loginAndGetInfo = loginAndGetInfo;
+
+emailCheck = function(email, callback){
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            callback(true);
+            return;
+        } else {
+            let sql = "SELECT COUNT(*) FROM UserCredentials WHERE email = "
+            sql += email;
+            // we are checking whether or not an email is being used
+            // FINISH THE QUERY ONCE DB IS CREATED
+            connection.query(sql, function(err, result) {
+                connection.release(); //release connection after it is used in order to not keep it open
+                if (err) {
+                    callback(true);
+                    return;
+                } else {
+                    callback(false, result);
+                }
+            });
+        }
+    });
+}
+module.exports.emailCheck = emailCheck;
+
+saveInfo = function(data, callback){
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            callback(true);
+            return;
+        } else {
+            let sql = "UPDATE ClientInformation SET fullName = ";
+            sql += data.fullName;
+            sql += ", address_1 = ";
+            sql += data.address1;
+            sql += ", address_2 = ";
+            sql += data.address2;
+            sql += ", city = ";
+            sql += data.city;
+            sql += ", state = ";
+            sql += data.state;
+            sql += ", zipCode = ";
+            sql += data.zipcode;
+            sql += ", historyExits = 0";
+            sql += "WHERE email = ";
+            sql += data.email;
+            // FINISH THE QUERY ONCE DB IS CREATED
+            connection.query(sql, function(err, result) {
+                connection.release(); //release connection after it is used in order to not keep it open
+                if (err) {
+                    callback(true);
+                    return;
+                } else {
+                    callback(false);
+                }
+            });
+        }
+    });
+}
+module.exports.saveInfo = saveInfo;
