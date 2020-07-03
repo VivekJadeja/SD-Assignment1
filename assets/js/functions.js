33128fd81c9bbe17a4089f3c2e449993acde74a3
Mayoorshardha <60448086+Mayoorshardha@users.noreply.github.com>
2020-07-03 11:45:27 -0500

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,7 +5,7 @@ function validateEmail(email) {
 module.exports.validateEmail = validateEmail;
 
 function validateInput(email, password) {
-    if ((validateEmail(data.email)) && (data.email !== "") && (data.password !== ""))
+    if ((validateEmail(email)) && (email !== "") && (password !== "") && (email) && (password))
         return true;
     else
         return false;
@@ -14,22 +14,22 @@ function validateInput(email, password) {
 module.exports.validateInput = validateInput;
 
 function validateRegisterProfile(dataObject) {
-    if (dataObject.fullName === "" || dataObject.fullName.length > 50)
+    if (!dataObject.fullName || dataObject.fullName === "" || dataObject.fullName.length > 50)
         return "Please enter a valid full name with appropiate length"
 
-    if (dataObject.address1 === "" || dataObject.address1.length > 100)
+    if (!dataObject.address1 || dataObject.address1 === "" || dataObject.address1.length > 100)
         return "Please enter a valid street address 1 with appropiate length"
 
-    if (dataObject.address2.length > 100)
+    if (dataObject.address2 && dataObject.address2.length > 100)
         return "Please enter a valid street address 2 with appropiate length"
 
-    if (dataObject.city === "" || dataObject.city.length > 100)
+    if (!dataObject.city || dataObject.city === "" || dataObject.city.length > 100)
         return "Please enter a valid city with appropiate length"
 
-    if (dataObject.state === "" || dataObject.state.length > 2)
+    if (!dataObject.state || dataObject.state === "" || dataObject.state.length > 2)
         return "Please enter a valid state"
 
-    if (dataObject.zipcode === "" || dataObject.zipcode.length < 5 || dataObject.zipcode.length > 9)
+    if (!dataObject.zipcode || dataObject.zipcode === "" || dataObject.zipcode.length < 5 || dataObject.zipcode.length > 9)
         return "Please enter a valid zip code with appropiate length"
 
     else
