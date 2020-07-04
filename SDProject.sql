8a39dd4703d4ce3927c44674e0de4e66b8a17a11
Mayoorshardha <60448086+Mayoorshardha@users.noreply.github.com>
2020-07-04 10:48:12 -0500

    Create SDProject.sql

---
+++ SDProject.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE OilCompany;
+
+USE OilCompany;
+
+
+CREATE TABLE ClientInformation
+(
+    email VARCHAR(50) NOT NULL,
+    fullName VARCHAR(50) NOT NULL,
+    address_1 VARCHAR(100) NOT NULL,
+    address_2 VARCHAR(100),
+    city VARCHAR(100) NOT NULL,
+    user_state VARCHAR(2) NOT NULL,
+    zipCode VARCHAR(9) NOT NULL,
+    historyExists int(1) NOT NULL,
+    PRIMARY KEY(email),
+    FOREIGN KEY(email) REFERENCES UserCredentials(email)
+);
+
