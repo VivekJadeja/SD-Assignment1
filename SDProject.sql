c1b966fa42c3bda8c598324042a7aa0cf3507c99
dkulinov <53202097+dkulinov@users.noreply.github.com>
2020-07-04 10:57:06 -0500

    added a db table

--- SDProject.sql
+++ SDProject.sql
@@ -3,23 +3,40 @@ CREATE DATABASE OilCompany;
 USE OilCompany;
 
 
-CREATE TABLE ClientInformation
-(
-    email VARCHAR(50) NOT NULL,
-    fullName VARCHAR(50) NOT NULL,
-    address_1 VARCHAR(100) NOT NULL,
-    address_2 VARCHAR(100),
-    city VARCHAR(100) NOT NULL,
-    user_state VARCHAR(2) NOT NULL,
-    zipCode VARCHAR(9) NOT NULL,
-    historyExists int(1) NOT NULL,
-    PRIMARY KEY(email),
-    FOREIGN KEY(email) REFERENCES UserCredentials(email)
-);
-
 CREATE TABLE UserCredentials
 (
     email VARCHAR(50) NOT NULL,
     user_password VARCHAR(100) NOT NULL,
     PRIMARY KEY(email)
 );
+
+CREATE TABLE ClientInformation
+(
+    email VARCHAR(50) NOT NULL,
+    fullName VARCHAR(50),
+    address_1 VARCHAR(100),
+    address_2 VARCHAR(100),
+    city VARCHAR(100),
+    user_state VARCHAR(2),
+    zipCode VARCHAR(9),
+    historyExists int(1),
+    PRIMARY KEY(email),
+    FOREIGN KEY(email) REFERENCES UserCredentials(email)
+);
+
+CREATE TABLE FuelQoute
+(
+    quoteID INT NOT NULL AUTO INCREMENT,
+    email VARCHAR(50) NOT NULL,
+    dateRequested TIMESTAMP NOT NULL DEFAULT NOW(),
+    deliveryDate DATE NOT NULL,
+    gallonsRequested INT NOT NULL,
+    pricePerGallon FLOAT NOT NULL,
+    PRIMARY KEY(quoteID),
+    FOREIGN KEY(email) REFERENCES UserCredentials(email)
+);
+
+
+CREATE TRIGGER insertIntoClientInfo AFTER INSERT ON UserCredentials
+ FOR EACH ROW
+	INSERT INTO ClientInformation(email) VALUES(NEW.email);
